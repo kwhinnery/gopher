90e27cd2bf20238e880c266d7ed6c906adb5e8b8
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-11-27 11:06:28 -0600

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Gopher Express
+
+[Express](http://expressjs.com) is the de-facto standard for a light weight web framework in node.js, but sometimes it might be nice to include just a tiny bit more in terms of default configuration and behavior. That's what Gopher Express is for.
+
+## Usage
+
+One goal was to provide useful default configuration, based on the opinions I became accustomed to in the [Sinatra world](http://www.sinatrarb.com/):
+
+    var app = require('gopher');
+
+    app.get('/', function(request, response) {
+        response.send('hello world!');
+    });
+
+This does a couple of things by default:
+
+* Creates a pre-configured express app as the base module object
+* Creates and starts an HTTP server with the given express app on `process.nextTick` (can override this with a config setting)
+
+## Configuration Choices
+
+Defaults to including the middleware you probably want, including POST data parsers for form-encoded and JSON bodies.
+
+## Dependencies
+
+Gopher assumes that you will use Express 3.x (as of this writing) and EJS as the default templating engine.
---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+// Create global app object
+var app = express();
+
+// Create an HTTP server for use with our app (and whatever else)
+var server = http.createServer(app);
+app.set('gopher.server', server);
+
+// Create some config that the user can override
+app.set('gopher.autostart', true);
+app.set('gopher.middleware', true);
+
+// Normal express config defaults
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(process.cwd(), 'views'));
+app.set('view engine', 'ejs');
+
+// Start the HTTP server with configured settings
+app.startServer = function() {
+    server.listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+};
+
+// Export the express app as the module interface
+module.exports = app;
+
+// Auto mount middleware and start the HTTP server on nextTick
+process.nextTick(function() {
+    if (app.get('gopher.middleware')) {
+        // Middleware stuff you probably want - TODO: Allow user to prevent 
+        // mounting specific middleware
+        app.use(express.logger('dev'));
+        app.use(express.json());
+        app.use(express.urlencoded());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(path.join(process.cwd(), 'public')));
+
+        if ('development' == app.get('env')) {
+            app.use(express.errorHandler());
+        }
+    }
+
+    // Auto-start HTTP server unless told otherwise...
+    if (app.get('gopher.autostart')) {
+        app.startServer();
+    }
+});
